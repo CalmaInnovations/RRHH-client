9ca5849d0aa3afbf61f49e248e50f042273ebb06
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2024-11-18 22:01:36 -0500

    actualizaciones faltantes

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -3,9 +3,11 @@ import { CssBaseline } from '@mui/material';
 import {purpleTheme} from '../theme';
 
 
+interface AppThemeProps {
+  children: React.ReactNode;
+}
 
-
-export const AppTheme = ({children}) => {
+export const AppTheme: React.FC<AppThemeProps> = ({children}) => {
   return (
     <ThemeProvider theme={ purpleTheme }>
         <CssBaseline/>
