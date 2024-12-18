92702f21e180f2de29d47ac620d36dc24248b890
Dennys <dennysmp64@gmail.com>
2024-12-17 23:27:19 -0500

    Responsive del SideBar

--- src/dashboard/layout/DashboardLayout.tsx
+++ src/dashboard/layout/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 import { Box, Toolbar } from "@mui/material";
-import { NavBar, SideBar } from "../components";
-import { ReactNode } from "react";
+import { NavBar, SideBar ,SideBarResponsive} from "../components";
+import { ReactNode, useEffect, useState } from "react";
 
 interface PropChildren {
    children: ReactNode;
@@ -9,12 +9,34 @@ interface PropChildren {
 const drawerWidth = 260;
 
 export const DashboardLayout = ({ children }: PropChildren) => {
+   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+   const [mobileOpen, setMobileOpen] = useState(false);
+
+   const handleDrawerToggle = () => {
+      setMobileOpen(!mobileOpen);
+   };
+   
+   useEffect(() => {
+      const handleResize = () => {
+         setWindowWidth(window.innerWidth);
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+         window.removeEventListener("resize", handleResize);
+      };
+   }, []);
    return (
       <Box sx={{ display: "flex", minHeight: "100vh", backgroundColor: "#F5F7FF" }}>
          {/* NavBar */}
-         <NavBar drawerWidth={drawerWidth} />
+         <NavBar drawerWidth={drawerWidth} handleDrawerToggle={handleDrawerToggle} />
          {/* Sidebar */}
-         <SideBar drawerWidth={drawerWidth} />
+         {windowWidth > 500 && <SideBar drawerWidth={drawerWidth} />}
+         {windowWidth < 500 && <SideBarResponsive mobileOpen={mobileOpen}
+            handleDrawerClose={handleDrawerToggle}
+            drawerWidth={240}/>}
+
          <Box
             component="main"
             sx={{ flexGrow: 1, p: 1, backgroundColor: "inherit" }}
