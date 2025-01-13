cd35c4132b542055f003ce0b83fb319450c91cb0
rafarth1 <rafarth1@outlook.com>
2025-01-12 22:41:39 -0500

    enviroment change in localhost

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,12 +1,8 @@
 export const environment = {
-    API_MASTER: "http://calmatest123.somee.com/",
+   //  API_MASTER: "http://calmatest123.somee.com/",
 
-<<<<<<< HEAD
    // API_MASTER: "http://localhost:8043",
-   // API_MASTER: "http://localhost:8080",
-=======
    API_MASTER: "http://localhost:8080",
->>>>>>> 69ff67df8e614732da7d077adc6cf1b517c87126
 
    // API_MASTER: "http://calmatest123.somee.com",
 };
