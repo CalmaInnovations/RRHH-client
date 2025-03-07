bf8dbe48710d02cbb8bb8cd02c9936fd191410d5
GulianaLulichacR <gulianalulichacr@gmail.com>
2025-03-07 13:33:43 -0500

    feat/agregar botón de editar para solicitar colarborador

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import { store } from "./app/store.ts";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+   <StrictMode>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </StrictMode>
 );
