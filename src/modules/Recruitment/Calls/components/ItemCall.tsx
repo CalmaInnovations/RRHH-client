dbd72ad777bcd74b4d5fc819fc055d8e1776d0cc
GulianaLulichacR <gulianalulichacr@gmail.com>
2025-03-10 14:00:21 -0500

    style/colores de titulos actualizados

--- src/modules/Recruitment/Calls/components/ItemCall.tsx
+++ src/modules/Recruitment/Calls/components/ItemCall.tsx
@@ -3,38 +3,40 @@ import { TbPointFilled } from "react-icons/tb";
 import { CallDetail } from "../interfaces/calls-interfaces";
 
 interface ItemProps {
-  call: CallDetail;
+   call: CallDetail;
 }
 
-const ItemCall = ({ call  }: ItemProps) => {
-  return (
-    <div className="bg-light p-4 rounded-md shadow-lg cursor-pointer transition-shadow duration-300 ease-in-out hover:shadow-2xl ">
-      <div className="flex items-center gap-1 text-grey-dark-ligth">
-        <TbPointFilled className="size-5" />
-        <h1>{call?.estadoSolicitud}</h1>
-      </div>
-      <div className="mt-5 ">
-        <h1 className="text-2xl font-semibold text-primary">Nombre del Puesto</h1>
-        <h3 className="font-medium text-grey-dark-ligth">{call?.modalidad}</h3>
-      </div>
+const ItemCall = ({ call }: ItemProps) => {
+   return (
+      <div className="bg-light p-4 rounded-md shadow-lg cursor-pointer transition-shadow duration-300 ease-in-out hover:shadow-2xl ">
+         <div className="flex items-center gap-1 text-heading-secondary">
+            <TbPointFilled className="size-5" />
+            <h2>{call?.estadoSolicitud}</h2>
+         </div>
+         <div className="mt-5 ">
+            <h1 className="text-2xl font-semibold text-primary">
+               Nombre del Puesto
+            </h1>
+            <h2>{call?.modalidad}</h2>
+         </div>
 
-      <div className="flex justify-between mt-2">
-        <div className="flex gap-2">
-          <h1 className="text-dark">Subárea:</h1>
-          <h1 className="text-grey"> {call?.nombreArea}</h1>
-        </div>
-        <div className="flex gap-2">
-          <h1 className="text-dark">Cantidad:</h1>
-          <h1 className="text-grey">{call?.cantidadSolicitada}</h1>
-        </div>
-      </div>
+         <div className="flex justify-between mt-2">
+            <div className="flex gap-2">
+               <h3>Subárea:</h3>
+               <h4> {call?.nombreArea}</h4>
+            </div>
+            <div className="flex gap-2">
+               <h3>Cantidad:</h3>
+               <h4>{call?.cantidadSolicitada}</h4>
+            </div>
+         </div>
 
-      <div className="flex justify-between">
-        <h1 className="text-dark">Reclutador(a) asginado:</h1>
-        <h1 className="text-grey">{call?.reclutador}</h1>
+         <div className="flex justify-between">
+            <h3>Reclutador(a) asginado:</h3>
+            <h4>{call?.reclutador}</h4>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default ItemCall;
