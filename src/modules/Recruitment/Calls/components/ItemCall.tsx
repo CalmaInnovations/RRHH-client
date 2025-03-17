fa2be0c4667349fd66b5985d36e250919815867e
GulianaLulichacR <gulianalulichacr@gmail.com>
2025-03-17 13:58:42 -0500

    Update ItemCall.tsx

--- src/modules/Recruitment/Calls/components/ItemCall.tsx
+++ src/modules/Recruitment/Calls/components/ItemCall.tsx
@@ -16,6 +16,7 @@ const ItemCall = ({ call }: ItemProps) => {
                <TbPointFilled className="size-5 text-heading-fourth" />
                <h4>{call?.estadoSolicitud}</h4>
             </div>
+
             {/* Title Section */}
             <div className="mt-3">
                <h1>Nombre del Puesto</h1>
@@ -37,32 +38,10 @@ const ItemCall = ({ call }: ItemProps) => {
                   </div>
                </div>
 
-               {/* Title Section */}
-               <div className="mt-3">
-                  <h1>Nombre del Puesto</h1>
-                  <h2 className="mt-1">{call?.modalidad}</h2>
-               </div>
-
-               {/* Details Section */}
-               <div className="mt-4 flex flex-col gap-3">
-                  <div className="flex items-center justify-between">
-                     <div className="flex items-baseline gap-2">
-                        <h3 className="mb-0">Subárea:</h3>
-                        <h4 className="leading-none">{call?.nombreArea}</h4>
-                     </div>
-                     <div className="flex items-baseline gap-2">
-                        <h3 className="mb-0">Cantidad:</h3>
-                        <h4 className="leading-none">
-                           {call?.cantidadSolicitada}
-                        </h4>
-                     </div>
-                  </div>
-
-                  {/* Recruiter Section */}
-                  <div className="flex items-baseline justify-between">
-                     <h3 className="mb-0">Reclutador(a) asignado:</h3>
-                     <h4 className="leading-none">{call?.reclutador}</h4>
-                  </div>
+               {/* Recruiter Section */}
+               <div className="flex items-baseline justify-between">
+                  <h3 className="mb-0">Reclutador(a) asignado:</h3>
+                  <h4 className="leading-none">{call?.reclutador}</h4>
                </div>
             </div>
          </div>
