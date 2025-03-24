3cf6913be7ae1bc244c109c1a491d44b52dd2514
carloslorap <81603256+carloslorap@users.noreply.github.com>
2025-03-24 15:55:17 -0500

    Revert "refactor: remove unused toastify CSS import"
    This reverts commit 1304778d982f47b6e9be2b96297641ad0d93231f.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import { store } from "./app/store.ts";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
